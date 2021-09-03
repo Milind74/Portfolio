0f67b6cff2083dcfcc24a06d59e3fe647731dd39
Milind <milindanand74@gmail.com>
2021-09-03 21:06:30 +0530

    portfolio

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,114 @@
+import React, { useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+
+import leaf from "../../Assets/Projects/Agoda.png";
+// import emotion from "../../Assets/Projects/emotion.jpeg";
+import editor from "../../Assets/Projects/Tripoto.png";
+import chatify from "../../Assets/Projects/ketto.png";
+// import suicide from "../../Assets/Projects/suicide.png";
+import bitsOfCode from "../../Assets/Projects/Codecov.png";
+import Aos from "aos";
+import "aos/dist/aos.css";
+function Projects() {
+  useEffect(() => {
+    Aos.init({ duration: 1200 });
+  });
+
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading" data-aos="fade-right">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }} data-aos="fade-up">
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={5} className="project-card" data-aos="fade-right">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="Ketto Clone "
+              description="Ketto is a well known crowdfunding platform where any individual across the country can raise funds for causes ranging from medical healthcare to disaster relief.
+
+              "
+              link="https://github.com/Ramlala-Yadav-Git/ketto-clone"
+              // linkmedium="https://nilp7744.medium.com/project-on-ketto-clone-5aceeccfc215"
+              // linklive="https://ketto-clone.netlify.app/"
+            />
+          </Col>
+
+          <Col md={5} className="project-card" data-aos="fade-up">
+            <ProjectCard
+              imgPath={leaf}
+              isBlog={false}
+              title="Agoda Clone"
+              description="Agoda.com is an online travel agency and metasearch engine for hotels, vacation rentals, flights, and airport transfer. It was founded in 2003 and later acquired by Booking Holdings, becoming a subsidiary of the company. About half of Agoda's bookings are made via its mobile apps."
+              link="https://github.com/Subham-Abhishek/Agoda-Clone"
+              // linkmedium="https://nilp7744.medium.com/project-on-clockify-clone-d6fa18977d4a"
+              // linklive="https://clockify-clonee.netlify.app/"
+            />
+          </Col>
+
+          <Col md={5} className="project-card" data-aos="fade-down">
+            <ProjectCard
+              imgPath={editor}
+              isBlog={false}
+              title="Tripo Clone"
+              description="Tripoto is a travel community and travel 
+              search engine portal based on user-generated content. 
+              The platform was launched on October 2013 by founders
+               Anirudh Gupta and Michael Pargal Lyngdoh. Tripoto allows users to write travel 
+              stories and discover those written by other travelers."
+              link="https://github.com/Milind74/heptane_tripoto"
+              // linkmedium=""
+              // linklive="https://foodxprs.netlify.app/order-online.html"
+            />
+          </Col>
+
+          <Col md={5} className="project-card" data-aos="fade-right">
+            <ProjectCard
+              imgPath={bitsOfCode}
+              isBlog={false}
+              title="Animoto Clone"
+              description="Codecov provides highly integrated tools
+               to group, merge, archive and compare coverage reports.
+              Uploads coverage reports from your CI. Merges builds and 
+              languages into a single coherent report. 
+              Displays coverage data in GitHub Files tab."
+              link="https://github.com/AbhishekSardagi/Codecov"
+              // linkmedium="https://nilp7744.medium.com/masai-collaboration-project-animoto-clone-fec2a84b2f8b"
+              // linklive="https://animoto-clone.netlify.app/"
+            />
+          </Col>
+
+          {/* <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={suicide}
+              isBlog={false}
+              title="Ai For Social Good"
+              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
+              link="https://github.com/soumyajit4419/AI_For_Social_Good"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={emotion}
+              isBlog={false}
+              title="Face Recognition and Emotion Detection"
+              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
+              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              link="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+            />
+          </Col> */}
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Particle from "../Particle";
+import Resumecontent from "./ResumeContent";
+import axios from "axios";
+import pdf from "../../Assets/milindresume.pdf";
+import { AiOutlineDownload } from "react-icons/ai";
+import Aos from "aos";
+import "aos/dist/aos.css";
+function Resume() {
+  const uri = "https://porfolio-backend.vercel.app/ranks/getRanks";
+  const [spojRank, upadteSpojRank] = useState(0);
+  const [hackerrank, upadteHackerank] = useState(0);
+  const [sem, upadateSem] = useState(0);
+  const [cgpa, upadteCgpa] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get(uri)
+      .then((res) => {
+        upadteSpojRank(res.data.message[0].spojRank);
+        upadteHackerank(res.data.message[1].hackerrank);
+        upadteCgpa(res.data.message[2].cgpa);
+        upadateSem(res.data.message[3].sem);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  });
+
+  return (
+    <Container fluid className="resume-section">
+      <Particle />
+      <Container>
+        <Row
+          style={{ justifyContent: "center", position: "relative" }}
+          data-aos="fade-up"
+        >
+          <Button variant="primary" href={pdf} target="_blank">
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+        <Row className="resume" data-aos="fade-up">
+          <Col md={6} className="resume-left">
+            <h3 className="resume-title">Training</h3>
+            <Resumecontent
+              title="JAVA TRAINING"
+              date="June 2020"
+              content={[
+                "Attended a training session in Haldia Institute of technology on java.",
+              ]}
+            />
+            <h3 className="resume-title">Extracurricular Activities</h3>
+            <Resumecontent
+              title="QUIZ COMPETITION  [ HIT-2018 ]"
+              content={[
+                "Participation in Quiz competition and Canvas Painting competitions of semi-online College fest 'Extravaganza' organized by my college. ",
+              ]}
+            />
+            <Resumecontent
+              title="Inter-Collegiate Cricket Tournament"
+              content={[
+                "Won 3 times  at Inter-Collegiate Cricket Tournament  ",
+              ]}
+            />
+          </Col>
+          <Col md={6} className="resume-right">
+            <h3 className="resume-title">Education</h3>
+            <Resumecontent
+              title=" B.E COMPUTER SCIENCE ENGINEERING [ haldia, west bengal ] "
+              date="2016 - 2020"
+              content={["CGPA: 7.11  "]}
+            />
+            <Resumecontent
+              title="12TH BOARD [ Milia Convent School ]"
+              date="2013 - 2015"
+              content={["Precentage: 52.54%"]}
+            />
+            <Resumecontent
+              title="10TH BOARD [ Mount Zion School ] "
+              date="2001 - 2012"
+              content={["Precentage: 90.00%"]}
+            />
+            {/* <h3 className="resume-title">Ranks and Achivements</h3> */}
+            {/* <Resumecontent
+              title=""
+              content={[
+                `Current rank in Spoj ${spojRank}`,
+                `Current rank in HackerRank  ${hackerrank}`,
+                "Top Performer in Code-Break 1.0",
+                "Participant in Hack-A-Bit 2019",
+              ]}
+            /> */}
+          </Col>
+        </Row>
+        <Row
+          style={{ justifyContent: "center", position: "relative" }}
+          data-aos="fade-right"
+        >
+          <Button variant="primary" href={pdf} target="_blank">
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Resume;
