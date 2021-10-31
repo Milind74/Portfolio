9b7a72530876ec2ffe3384621fa3114d49e6a193
Milind <milindanand74@gmail.com>
2021-10-31 16:34:05 +0530

    slight change

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -6,8 +6,7 @@ import { SiRedux } from "react-icons/si";
 import {
   DiJavascript1,
   DiReact,
-  DiNodejs,
-  DiMongodb,
+  
   DiGit,
   
 } from "react-icons/di";
@@ -23,18 +22,14 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
+      
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiRedux />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
+      
       
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
